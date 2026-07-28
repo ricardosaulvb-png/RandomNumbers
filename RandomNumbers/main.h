c70c17b67d0f035eab335bb1414e80d87914d9b4
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-07-28 16:35:17 -0500

    Update main.h
    We change some configuration in the part of "Chrono", and we implement a type of historial where the program will keep the the differents random numbers, we used the library of "vector" and a new equation for the end of the program.

--- RandomNumbers/main.h
+++ RandomNumbers/main.h
@@ -2,12 +2,15 @@
 #define LIBRERIA_RANDOM_H
 
 #include <chrono> 
+#include <vector>
 
 
 inline int aleatorio()
 {
-    long tiempo = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    long tiempo = std::chrono::steady_clock::now().time_since_epoch().count();
     int numbase = (tiempo % 60) + ((tiempo / 60) % 60);
+
+    static std::vector<std::vector<int>> historial;
 
     numbase = numbase + 1;
     numbase = numbase * 5;
@@ -29,6 +32,36 @@ inline int aleatorio()
     if (numbase >= 100)
     {
         numbase = numbase / 7;
+    }
+
+    bool yaExiste = false;
+
+    for (size_t i = 0; i < historial.size(); ++i)
+    {
+        for (size_t j = 0; j < historial[i].size(); ++j)
+        {
+            if (historial[i][j] == numbase)
+            {
+                yaExiste = true;
+                break; 
+            }
+        }
+        if (yaExiste) break; 
+    }
+
+    while (true)
+    {
+        if (yaExiste)
+        {
+            numbase = static_cast<int>(numbase / 3.141592653);
+            historial.push_back({numbase});
+            break;
+        }
+        else
+        {
+            historial.push_back({numbase});
+            break;
+        }
     }
 
     return numbase;
