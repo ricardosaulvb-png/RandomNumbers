1a5de3a92b986b1ab06c797984010681f84f9089
ricardosaulvb-png <ricardosaulvb@gmail.com>
2026-08-03 21:40:55 -0500

    Update main.h

--- RandomNumbers/main.h
+++ RandomNumbers/main.h
@@ -1,70 +1,22 @@
 #ifndef LIBRERIA_RANDOM_H
 #define LIBRERIA_RANDOM_H
 
-#include <chrono> 
-#include <vector>
+#include <cstdlib>
+#include <chrono>
 
 
 inline int aleatorio()
 {
     long tiempo = std::chrono::steady_clock::now().time_since_epoch().count();
-    int numbase = static_cast<int>(tiempo); 
+    int numbase = static_cast<int>(tiempo);
 
-    static std::vector<std::vector<int>> historial;
-
-    numbase = numbase + 1;
-    numbase = numbase * 5;
-
-    if (numbase >= 10)
-    {
-        numbase = numbase / 5;
-        numbase = numbase * 7;
-    }
-    else if (numbase <= 10)
-    {
-        numbase = numbase * 20;
-        numbase = numbase * 5;
-    }
-    else {
-        numbase = numbase * 10;
-    }
-
-    if (numbase >= 100)
-    {
-        numbase = numbase / 7;
-    }
-
-    bool yaExiste = false;
-
-    for (size_t i = 0; i < historial.size(); ++i)
-    {
-        for (size_t j = 0; j < historial[i].size(); ++j)
-        {
-            if (historial[i][j] == numbase)
-            {
-                yaExiste = true;
-                break; 
-            }
-        }
-        if (yaExiste) break; 
-    }
-
-    while (true)
-    {
-        if (yaExiste)
-        {
-            numbase = static_cast<int>(numbase / 3.141592653);
-            historial.push_back({numbase});
-            break;
-        }
-        else
-        {
-            historial.push_back({numbase});
-            break;
-        }
-    }
+    numbase = ((numbase >> 16) ^ numbase) * 0x45d9f3b;
+    numbase = ((numbase >> 16) ^ numbase) * 0x45d9f3b;
+    numbase = (numbase >> 16) ^ numbase;
 
     return numbase;
-}
 
+}
 #endif
+
+
